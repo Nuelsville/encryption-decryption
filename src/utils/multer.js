effd96e37779703b8f4a1f6dd8c83de6fa50b007
Nuelsville <nuelsville@gmail.com>
2025-06-05 15:57:04 +0100

    all tyoes

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -16,20 +16,43 @@ const storage = multer.diskStorage({
 
 // File filter
 const fileFilter = (req, file, cb) => {
-
+  // Define allowed MIME types inlcude docs, spreeds, and other common file types
     const allowedTypes = [
         "application/pdf",
+        "application/msword",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/vnd.ms-excel",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        "application/vnd.ms-powerpoint",
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        "application/rtf",
+        "application/xml",
+        "application/zip",
+        "application/x-zip-compressed",
+        "application/x-rar-compressed",
+        "image/gif",
         "image/png",
         "image/jpeg",
         "text/plain",
+        "text/csv",
+        "text/html",
+        "text/css",
+        "text/javascript",
         "application/json",
-        "application/octet-stream"
+        "application/octet-stream",
+        "application/zip",
+        "application/x-zip-compressed",
+        "application/x-rar-compressed",
+        "application/x-tar",
+        "application/x-7z-compressed",
+        "application/x-bzip",
     ];
       
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Unsupported file type."), false);
+    cb(null, true);
+    // cb(new Error("Unsupported file type."), false);
   }
 };
 
